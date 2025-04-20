41359e010b7e17b339e7d153eac97c2bbf993f8e
Gayan Benipal <gayanbenipal@Gayans-MacBook-Air.local>
2025-04-20 13:52:22 +0530

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-brand-900">
       <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8 text-center">
-        <p className="text-white font-light tracking-wide">© 2025 GB Global. All rights reserved.</p>
+        <p className="text-white font-light tracking-wide">© 2025 GB GLOBA<span className="relative">L<sup className="absolute -top-2 right-0 text-sm text-white">®</sup></span>. All rights reserved.</p>
       </div>
     </footer>
   );
